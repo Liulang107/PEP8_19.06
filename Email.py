5c2c7cce1f624840ad4ba035b3951ef79e2ed70b
Zolotova Maria <liulang107@gmail.com>
2019-06-24 10:16:53 +0300

    errors fixed

--- Email.py
+++ Email.py
@@ -7,6 +7,7 @@ from email.mime.multipart import MIMEMultipart
 GMAIL_SMTP = "smtp.gmail.com"
 GMAIL_IMAP = "imap.gmail.com"
 
+
 class Email:
     def __init__(self, login, password, subject, recipients, message, header):
         self.login = login
@@ -14,7 +15,7 @@ class Email:
         self.subject = subject
         self.recipients = recipients
         self.message = message
-        if header != True:
+        if header is not True:
             header = 'ALL'
         self.header = header
 
@@ -39,18 +40,19 @@ class Email:
         ms.sendmail(self.login, ms, msg.as_string())
         ms.quit()
 
-    def recieve_message(self):
+    def receive_message(self):
         mail = imaplib.IMAP4_SSL(GMAIL_IMAP)
         mail.login(self.login, self.password)
         mail.list()
         mail.select("inbox")
-        criterion = f'(HEADER Subject "{header}")'
+        criterion = f'(HEADER Subject "{self.header}")'
         result, data = mail.uid('search', None, criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
         result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
         raw_email = data[0][1]
         email_message = email.message_from_string(raw_email)
+        print(email_message)
         mail.logout()
 
 
